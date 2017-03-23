2891952b60da8ff6a6bc25167b7c92dca66e5542
Charles Allen <charles@allen-net.com>
2017-03-22 22:16:11 -0700

    Fix retry logic to eventually quit

--- src/main/java/com/sumologic/log4j/http/SumoHttpSender.java
+++ src/main/java/com/sumologic/log4j/http/SumoHttpSender.java
@@ -100,7 +100,7 @@ public class SumoHttpSender
           break;
         }
       }
-    } while (!success && !Thread.currentThread().isInterrupted());
+    } while (!success && !Thread.currentThread().isInterrupted() && nTry < 5);
   }
 
   private void trySend(byte[] body) throws IOException
